927732b6ad05ead325773edf23e5c86cdbb87a46
Dmytro Beliasnyk <beliasnykdmytro@gmail.com>
2024-10-02 14:12:07 +0200

    refactoring main and add shutting down

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,17 @@
 package main
 
 import (
+	"context"
 	"log"
 	"os"
+	"os/signal"
+	"syscall"
 
-	"github.com/DmytroBeliasnyk/crud_app_rest_api/pkg/datasource"
+	"github.com/DmytroBeliasnyk/crud_app_rest_api/core"
+	"github.com/DmytroBeliasnyk/crud_app_rest_api/pkg/handlers"
+	"github.com/DmytroBeliasnyk/crud_app_rest_api/pkg/repositories"
+	"github.com/DmytroBeliasnyk/crud_app_rest_api/pkg/services"
+	"github.com/jmoiron/sqlx"
 	"github.com/joho/godotenv"
 	"github.com/spf13/viper"
 )
@@ -18,7 +25,39 @@ func main() {
 		log.Fatalf("error loading env variables: %s", err.Error())
 	}
 
-	cfg := datasource.Config{
+	db, err := initDB()
+	if err != nil {
+		log.Fatalf("error occurred while connecting to db: %s", err.Error())
+	}
+
+	repo := repositories.NewRepository(db)
+	service := services.NewService(repo)
+	handlers := handlers.NewHandler(service)
+
+	server := new(core.Server)
+	go func() {
+		if err = server.Run(viper.GetString("port"), handlers.InitRoutes()); err != nil {
+			log.Fatalf("error occurred while running http server: %s", err.Error())
+		}
+	}()
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	<-quit
+
+	if err = server.Shutdown(context.Background()); err != nil {
+		log.Fatalf("error occurred on server shutting down: %s", err.Error())
+	}
+
+	err = db.Close()
+	if err != nil {
+		log.Fatalf("error occurred on db connection close: %s", err.Error())
+	}
+}
+
+func initDB() (*sqlx.DB, error) {
+	cfg := repositories.Config{
 		Host:     viper.GetString("db.host"),
 		Port:     viper.GetString("db.port"),
 		Username: viper.GetString("db.username"),
@@ -27,15 +66,7 @@ func main() {
 		SSLMode:  viper.GetString("db.sslmode"),
 	}
 
-	db, err := datasource.NewPostgresDB(cfg)
-	if err != nil {
-		log.Fatalf("error occurred while connecting to db: %s", err.Error())
-	}
-
-	err = db.Close()
-	if err != nil {
-		log.Fatalf("error occurred while close db: %s", err.Error())
-	}
+	return repositories.NewPostgresDB(cfg)
 }
 
 func initConfig() error {
